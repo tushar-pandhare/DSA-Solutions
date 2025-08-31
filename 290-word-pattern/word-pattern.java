d67b8d825b70e95eaf996a3452f9a6b0d074c141
Tushar Pandhare <tusharpandharetp@gmail.com>
2025-09-01 02:02:27 +0530

    Time: 1 ms (79.48%) | Memory: 41.3 MB (83.12%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) return false;
+
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Character> reverseMap = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if (map.containsKey(ch) && !map.get(ch).equals(word)) return false;
+            if (reverseMap.containsKey(word) && reverseMap.get(word) != ch) return false;
+
+            map.put(ch, word);
+            reverseMap.put(word, ch);
+        }
+
+        return true;
+    }
+}
