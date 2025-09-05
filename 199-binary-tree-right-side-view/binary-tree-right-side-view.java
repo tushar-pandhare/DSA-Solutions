d7c160cba65a71cd7bef121aeb06ac0d6d0781f2
Tushar Pandhare <tusharpandharetp@gmail.com>
2025-09-05 16:44:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (63.68%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,32 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    List<Integer> res=new ArrayList<>();
+    List<Integer> res = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
-        HashSet<Integer> set=new HashSet<>();
-        // set.add(0);
-        traverse(root,set,0);
+        helper(root, 0);
         return res;
     }
-    public void traverse(TreeNode root,HashSet<Integer> set,int level){
+    public void helper(TreeNode root, int level) {
         if(root==null) return;
-        if(set.add(level)){
-            res.add(root.val);
-        }
-        traverse(root.right,set,level+1);
-        traverse(root.left,set,level+1);  
+        if(res.size()==level) res.add(root.val);
+        helper(root.right,level+1);
+        helper(root.left,level+1);
     }
 }
