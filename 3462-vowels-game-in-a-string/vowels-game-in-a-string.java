7d110c3f0948adf910973919c26364279ade3696
Tushar Pandhare <tusharpandharetp@gmail.com>
2025-09-12 12:35:07 +0530

    Time: 7 ms (55.38%) | Memory: 45.5 MB (82.68%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int vc=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                vc++;
+            }
+        }
+        return vc>0;
+    }
+}
