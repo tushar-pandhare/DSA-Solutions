8a7b9754b6a6fd54756445a32d534235664e52d9
Tushar Pandhare <tusharpandharetp@gmail.com>
2025-10-13 00:17:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Hello World/c-hello-world.cpp
@@ -0,0 +1,12 @@
+
+
+class Solution {
+  public:
+    void helloWorld() {
+
+        // Type below cout<<"Hello World";
+        cout<<"Hello World";
+        // cout will output to console
+        
+    }
+};
