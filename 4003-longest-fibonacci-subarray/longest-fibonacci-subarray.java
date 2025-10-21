577bc22038e9b470a8a0531883173bfb37f10b7b
Tushar Pandhare <tusharpandharetp@gmail.com>
2025-10-21 22:27:08 +0530

    Time: 2 ms (32.81%) | Memory: 58.4 MB (35.51%) - LeetSync

--- 4003-longest-fibonacci-subarray/longest-fibonacci-subarray.java
+++ 4003-longest-fibonacci-subarray/longest-fibonacci-subarray.java
@@ -2,7 +2,7 @@ class Solution {
     public int longestSubarray(int[] nums) {
         int maxi=2;
         int curr=2;
-        for(int i=3-1;i+1<=nums.length;++i){
+        for(int i=2;i+1<=nums.length;++i){
             if(nums[i-1]+nums[i-2]==nums[i]){
                 maxi=Math.max(++curr,maxi);
             }
