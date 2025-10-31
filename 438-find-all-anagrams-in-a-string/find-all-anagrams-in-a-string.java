eaf006ae4eb1bdeef9cef9bb0392f087d60caede
Tushar Pandhare <tusharpandharetp@gmail.com>
2025-11-01 01:10:10 +0530

    Time: 9 ms (90.52%) | Memory: 46.5 MB (5.29%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,37 +1,57 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int start=0,end=0;
-        List<Integer> ans=new ArrayList<>();
-        HashMap<Character,Integer> pMap=new HashMap<>(); 
-        HashMap<Character,Integer> sMap=new HashMap<>();
+        // int start=0,end=0;
+        // List<Integer> ans=new ArrayList<>();
+        // HashMap<Character,Integer> pMap=new HashMap<>(); 
+        // HashMap<Character,Integer> sMap=new HashMap<>();
 
-        //storing original string in set
-        for(Character ch: p.toCharArray()){
-            pMap.put(ch,pMap.getOrDefault(ch,0)+1);
-        } 
+        // //storing original string in set
+        // for(Character ch: p.toCharArray()){
+        //     pMap.put(ch,pMap.getOrDefault(ch,0)+1);
+        // } 
 
-        //checking anagrams      
-        while(end<s.length()){
+        // //checking anagrams      
+        // while(end<s.length()){
             
-            //adding each letter to map2
-            char ch=s.charAt(end); 
-            if(pMap.containsKey(ch)){
-                sMap.put(ch,sMap.getOrDefault(ch,0)+1);
-            }
+        //     //adding each letter to map2
+        //     char ch=s.charAt(end); 
+        //     if(pMap.containsKey(ch)){
+        //         sMap.put(ch,sMap.getOrDefault(ch,0)+1);
+        //     }
 
-            if(end-start+1 > p.length()){
-                char lchar=s.charAt(start++);
-                if(sMap.containsKey(lchar)){
-                    if(sMap.get(lchar)==1) sMap.remove(lchar);
-                    else sMap.put(lchar,sMap.get(lchar)-1);
-                }
-            }
+        //     if(end-start+1 > p.length()){
+        //         char lchar=s.charAt(start++);
+        //         if(sMap.containsKey(lchar)){
+        //             if(sMap.get(lchar)==1) sMap.remove(lchar);
+        //             else sMap.put(lchar,sMap.get(lchar)-1);
+        //         }
+        //     }
 
-            if(pMap.equals(sMap) && end-start+1==p.length()){
-                ans.add(start);
-            }
+        //     if(pMap.equals(sMap) && end-start+1==p.length()){
+        //         ans.add(start);
+        //     }
         
-            end++;
+        //     end++;
+        // }
+        // return ans;
+
+        // Code 2 O(1)
+        List<Integer> ans=new ArrayList<>();
+        if(s.length()<p.length()) return ans;
+        int[] pFreq=new int[26];
+        int[] sFreq=new int[26];
+        for(char ch:p.toCharArray()){
+            pFreq[ch-'a']++;
+        }
+        int window=p.length();
+        for(int i=0;i<s.length();i++){
+            sFreq[s.charAt(i)-'a']++;
+            if(i>=window){
+                sFreq[s.charAt(i-window)-'a']--;
+            }
+            if(Arrays.equals(pFreq,sFreq)){
+                ans.add(i-window+1);
+            }
         }
         return ans;
     }
